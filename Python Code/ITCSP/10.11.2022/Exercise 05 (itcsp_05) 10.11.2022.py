ceaaf88fa86351176ccbfcc3b41a98fdfaf7778b
MuratcG <muratcangumuslu@gmail.com>
2022-11-11 22:18:14 +0100

    adds the exercise file

---
+++ Python Code/ITCSP/10.11.2022/Exercise 05 (itcsp_05) 10.11.2022.py
@@ -0,0 +1,8 @@
+# Write a program that will calculate the surface area and print it to the terminal
+# Take radius as 5, pi as 3.14
+# A = pi.r ^ 2 is the formula
+
+radius = 5
+pi = 3.14
+circleArea = pi * (radius ** 2)
+print(circleArea)
