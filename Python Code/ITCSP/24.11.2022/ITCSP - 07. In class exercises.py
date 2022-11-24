70bb6c12998fd81694e285297cb271ead400ae23
MuratcG <muratcangumuslu@gmail.com>
2022-11-24 09:26:38 +0100

    adds the given date's in class exercises

---
+++ Python Code/ITCSP/24.11.2022/ITCSP - 07. In class exercises.py
@@ -0,0 +1,33 @@
+# Write a program that asks the user for their first name, birth year and the current year, and then calculates their age.
+# Print the result in the console as "Xyz, you are 12 years old."
+
+# firstName = input("Type your name\n")
+# birthYear = input("Type your birth year\n")
+# currentYear = input("Type the current year\n")
+
+# age = (int(currentYear) - int(birthYear))
+# myStr = f"{firstName}, you are {age} years old."
+# print(myStr)
+
+# meal = "fruit"
+# money = 0
+# isLunchDelivered = (meal == "fruit" or meal == "sandwhich") and money >= 2
+# print(isLunchDelivered)
+
+#if 2+2 ==4:
+#    print("that was true")
+
+#if 2+2 == 4:
+#    print("that was true")
+#    print("really true")
+
+#if 2 + 2 == 4:
+#    print("That shouldn't happen")
+#    print('That shouldn\'t happen, using escape character')
+#    print("or this....")
+
+#if 2+2 == 5:
+#    print("this won't print")
+#else:
+#    print("but this will")
+#    print("and so will this")
